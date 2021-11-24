b5df704c18a1ba0524acb4122ee6abe81ee20aa4
rhizoma-atractylodis <atractylodis@gmail.com>
2021-11-24 22:04:16 +0800

    original digits

---
+++ src/originalDigits/OriginalDigits.java
@@ -0,0 +1,21 @@
+package originalDigits;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OriginalDigits {
+    public static void main(String[] args) {
+        String s = "owoztneoer";
+        Map<String, Integer> dict = new HashMap<>();
+        dict.put("zero", 0);
+        dict.put("one", 1);
+        dict.put("two", 2);
+        dict.put("three", 3);
+        dict.put("four", 4);
+        dict.put("five", 5);
+        dict.put("six", 6);
+        dict.put("seven", 7);
+        dict.put("eight", 8);
+        dict.put("nine", 9);
+    }
+}
